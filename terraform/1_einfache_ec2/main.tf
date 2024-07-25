cd640b5400ba5f15f260c41c17961f63be132c84
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-07-25 11:59:28 +0200

    Korrektur

--- terraform/1_einfache_ec2/main.tf
+++ terraform/1_einfache_ec2/main.tf
@@ -25,13 +25,5 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-# Eine Ubuntu EC2 Instanz im kleinen t2.nano-Format
-resource "aws_instance" "erste_instanz" {
-  instance_type = "t2.nano"
-  ami = "ami-01e444924a2233b07"
-  vpc_security_group_ids = [ "sg-0a3fe4f9a1c13ef19" ]
-  tags = {
-    Name = "Terraform Beispiel"
-  }
-}
+
 
